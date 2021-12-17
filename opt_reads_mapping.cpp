82bc1ff9b6f92894b12943646c92abac1c4e208f
ivanvikhrev <l4ind@mail.ru>
2021-12-17 11:03:28 +0300

    init

---
+++ opt_reads_mapping.cpp
@@ -0,0 +1,111 @@
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+#include <map>
+#include <string>
+#include <vector>
+#include <omp.h>
+
+size_t ham_dist(const char* s1, const char* s2, size_t len) {
+    size_t dist = 0;
+    for (size_t i = 0; i < len; ++i) {
+        dist += (s1[i] != s2[i]);
+    }
+
+    return dist;
+}
+
+std::map<std::string, std::vector<size_t>> build_kmers_map(const std::string& genome, size_t kmer_len) {
+    std::map<std::string, std::vector<size_t>> kmers;
+    for (size_t i = 0; i < genome.size() - kmer_len + 1; ++i) {
+        std::string kmer = genome.substr(i, kmer_len);
+        if (kmers.find(kmer) == kmers.end()) {
+            kmers.insert({ kmer, {i} });
+        }
+        else {
+            kmers[kmer].push_back(i);
+        }
+    }
+
+    return kmers;
+}
+
+std::vector<int> simple_reads_mapping(const std::string& genome, const std::vector<std::string>& reads, size_t kmer_len) {
+    std::vector<int> mapped_reads(reads.size());
+    std::map<std::string, std::vector<size_t>> kmers_map = build_kmers_map(genome, kmer_len);
+    auto genome_cstr = genome.c_str();
+
+    size_t count = 0;
+    for (const auto& read : reads) {
+        bool found = false;
+
+        std::string seed = read.substr(0, kmer_len);
+        auto seed_it = kmers_map.find(seed);
+        std::vector<size_t> poss = {};
+
+        if (seed_it != kmers_map.end()) {
+            poss = seed_it->second;
+        }
+
+        for (size_t pos : poss) {
+            if (pos + read.size() < genome.size()) {
+                if (std::strncmp(read.c_str(), genome_cstr + pos, read.size()) == 0) {
+                    mapped_reads[count] = int(pos);
+                    found = true;
+                }
+            }
+        }
+
+        if (!found) {
+            mapped_reads[count] = -1; // no perfect match for read in genome
+        }
+        ++count;
+    }
+
+    return mapped_reads;
+}
+
+int main() {
+    std::ifstream genome_file;
+    std::ifstream reads_file;
+
+    //std::string genome_file_name = "test_example/genome.fasta";
+    std::string genome_file_name = "genome.fasta";
+    genome_file.open(genome_file_name);
+    if (!genome_file) {
+        throw std::runtime_error("Unable to open file " + genome_file_name);
+    }
+
+    //std::string reads_file_name = "test_example/reads.txt";
+    std::string reads_file_name = "reads.txt";
+    reads_file.open(reads_file_name);
+    if (!reads_file) {
+        throw std::runtime_error("Unable to open file " + reads_file_name);
+    }
+
+    std::string line;
+    std::string genome = "";
+    std::getline(genome_file, line); // skip header
+    while (std::getline(genome_file, line)) {
+        genome += line;
+    }
+    genome_file.close();
+
+    std::vector<std::string> reads;
+    while (std::getline(reads_file, line)) {
+        reads.push_back(line);
+    }
+    reads_file.close();
+    auto mapped_reads = simple_reads_mapping(genome, reads, 3);
+
+
+    std::ofstream res("res.txt");
+    for (size_t i = 0; i < mapped_reads.size(); ++i) {
+        res << reads[i] << " " << mapped_reads[i] << '\n';
+    }
+
+    res.close();
+
+    return 0;
+}
---
+++ unopt_reads_mapping.cpp
@@ -0,0 +1,116 @@
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+#include <map>
+#include <string>
+#include <vector>
+
+size_t ham_dist(const std::string& s1, const std::string& s2) {
+    if (s1.size() != s2.size()) {
+        throw std::logic_error("Hamming distance assumes that string lengths are equal!");
+    }
+
+    size_t dist = 0;
+    for (size_t i = 0; i < s1.size(); ++i) {
+        if (s1[i] != s2[i]) {
+            ++dist;
+        }
+    }
+
+    return dist;
+}
+
+std::map<std::string, std::vector<size_t>> build_kmers_map(const std::string& genome, size_t kmer_len) {
+    std::map<std::string, std::vector<size_t>> kmers;
+    size_t pos = 0;
+    for (size_t i = 0; i < genome.size() - kmer_len + 1; ++i) {
+        std::string kmer = genome.substr(i, kmer_len);
+        if (kmers.find(kmer) == kmers.end()) {
+            kmers.insert({kmer, {i}});
+        } else {
+            kmers[kmer].push_back(i);
+        }
+    }
+
+    return kmers;
+}
+
+std::map<std::string, int> simple_reads_mapping(const std::string& genome, std::vector<std::string> reads, size_t kmer_len) {
+    std::map<std::string, int> mapped_reads;
+    std::map<std::string, std::vector<size_t>> kmers_map = build_kmers_map(genome, kmer_len);
+    size_t count = 0;
+    for (const auto& read : reads) {
+        if (count % 10000 == 0) {
+            std::cout << count << " reads were processed" << std::endl;
+        }
+
+        std::string seed = read.substr(0, kmer_len);
+        bool found = false;
+        auto seed_it = kmers_map.find(seed);
+        std::vector<size_t> poss = {};
+
+        if (seed_it != kmers_map.end()) {
+            poss = seed_it->second;
+        }
+
+        for (size_t pos : poss) {
+            if (pos + read.size() < genome.size()) {
+                size_t d = ham_dist(read, genome.substr(pos, read.size()));
+                if (d == 0) {
+                    mapped_reads.insert({read, static_cast<int>(pos)});
+                    found = true;
+                }
+            }
+        }
+
+        if (!found) {
+            mapped_reads.insert({ read, -1 }); // no perfect match for read in genome
+        }
+        ++count;
+    }
+
+    return mapped_reads;
+}
+
+int main() {
+    std::ifstream genome_file;
+    std::ifstream reads_file;
+
+    //std::string genome_file_name = "test_example/genome.fasta";
+    std::string genome_file_name = "genome.fasta";
+    genome_file.open(genome_file_name );
+    if (!genome_file) {
+        throw std::runtime_error("Unable to open file " + genome_file_name);
+    }
+
+    //std::string reads_file_name = "test_example/reads.txt";
+    std::string reads_file_name = "reads.txt";
+    reads_file.open(reads_file_name );
+    if (!reads_file) {
+        throw std::runtime_error("Unable to open file " + reads_file_name);
+    }
+
+    std::string line;
+    std::string genome = "";
+    std::getline(genome_file, line); // skip header
+    while(std::getline (genome_file, line)) {
+      genome += line;
+    }
+    genome_file.close();
+
+    std::vector<std::string> reads;
+    while (std::getline(reads_file, line)) {
+      reads.push_back(line);
+    }
+    reads_file.close();
+    auto mapped_reads = simple_reads_mapping(genome, reads, 3);
+
+
+    std::ofstream res("../../res.txt");
+    for (const auto& item : mapped_reads) {
+        res << item.first << " " << item.second << std::endl;
+    }
+
+    res.close();
+}
